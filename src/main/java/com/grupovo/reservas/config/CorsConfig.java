78d30804881aafc904b2b6d3fc096453d5374e64
BenyiCordero <benyi.uriel2006@gmail.com>
2026-06-05 17:35:17 -0600

    CORS

--- src/main/java/com/grupovo/reservas/config/CorsConfig.java
+++ src/main/java/com/grupovo/reservas/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                    .allowedOrigins("http://127.0.0.1:5500", "http://localhost:5501", "http://localhost:5500")
+                    .allowedOrigins("http://127.0.0.1:5500", "http://localhost:5501", "http://localhost:5500", "https://cal.smarttech.icu")
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                     .allowedHeaders("*")
                     .allowCredentials(true);
